a6fa2f885f5535a48b8a9a079bc38468458f20dd
Teresa L <tml2009jghs@yahoo.com>
2022-06-07 20:08:55 -0400

    Add logic to logout if token is expired

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link, useHistory, useLocation } from "react-router-dom";
 import { AppBar, Avatar, Typography, Toolbar, Button} from "@material-ui/core";
 import { useDispatch } from "react-redux";
+import decode from "jwt-decode";
 import useStyles from "./styles";
 
 const Navbar = () => {
@@ -23,7 +24,11 @@ const Navbar = () => {
     useEffect(() => {
         const token = user?.token;
 
-        // JWT ...
+        if(token) {
+            const decodedToken = decode(token);
+
+            if(decodedToken.exp * 1000 < new Date().getTime()) logout();
+        }
 
         setUser(JSON.parse(localStorage.getItem("profile")))
     }, [location]);
