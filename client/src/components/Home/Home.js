44b0828ae8018ebeabe3ad2fbb5da2b1aeb9f304
Teresa L <tml2009jghs@yahoo.com>
2022-06-08 20:29:16 -0400

    Create Pagination component

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { Container, Grow, Grid } from "@material-ui/core";
+import { Container, Grow, Grid, Paper } from "@material-ui/core";
 import { useDispatch } from "react-redux";
 
 import { getRecipes } from "../../actions/recipes";
+import Pagination from "../Pagination";
 import Recipes from "../Recipes/Recipes";
 import Form from "../Form/Form";
 import useStyles from "./styles";
@@ -20,12 +21,15 @@ const Home = () => {
     return (
         <Grow in>
             <Container>
-                <Grid className={classes.mainContainer} container justifyContent="space-between" alignItems="stretch" spacing={3}>
+                <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
                     <Grid item xs={12} sm={7}>
                         <Recipes setCurrentId={setCurrentId} />
                     </Grid>
                     <Grid item xs={12} sm={4}>
                         <Form currentId={currentId} setCurrentId={setCurrentId} />
+                        <Paper elevation={6}>
+                            <Pagination />
+                        </Paper>
                     </Grid>
                 </Grid>
             </Container>
