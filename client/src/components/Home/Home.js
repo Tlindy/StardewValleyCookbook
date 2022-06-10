75ccae95fa0ea45c438f5d29f363fd794eeb35b8
Teresa L <tml2009jghs@yahoo.com>
2022-06-09 21:44:05 -0400

    Add search fields to Home

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { Container, Grow, Grid, Paper } from "@material-ui/core";
+import { Container, Grow, Grid, Paper, AppBar, TextField, Button } from "@material-ui/core";
 import { useDispatch } from "react-redux";
+import { useHistory, useLocation } from "react-router-dom";
+import ChipInput from "material-ui-chip-input";
 
 import { getRecipes } from "../../actions/recipes";
 import Pagination from "../Pagination";
@@ -8,24 +10,88 @@ import Recipes from "../Recipes/Recipes";
 import Form from "../Form/Form";
 import useStyles from "./styles";
 
+function useQuery() {
+    return new URLSearchParams(useLocation().search);
+}
+
 const Home = () => {
 
     const [currentId, setCurrentId] = useState(null);
     const classes = useStyles();
     const dispatch = useDispatch();
+    const query = useQuery();
+    const history = useHistory();
+    const page = query.get("page") || 1;
+    const searchQuery = query.get("searchQuery");
+    const [search, setSearch] = useState("");
+    const [ingredients, setIngredients] = useState([]);
+
 
     useEffect(() => {
         dispatch(getRecipes());
     }, [currentId, dispatch]);
 
+    const searchRecipe = () => {
+        if(search.trim()) {
+            // dispatch -> fetch search post
+        } else {
+            history.push("/");
+        }
+    };
+
+    const handleKeyPress = (e) => {
+        if(e.keyCode === 13) {
+            searchRecipe();
+        }
+    };
+
+    const handleAdd = (ingredient) => setIngredients([...ingredients, ingredient]);
+
+    const handleDelete = (ingredientToDelete) => {
+        setIngredients(ingredients.filter((ingredient) => ingredient !== ingredientToDelete));
+    }
+
     return (
         <Grow in>
-            <Container>
-                <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}>
-                    <Grid item xs={12} sm={7}>
+            <Container maxWidth="xl">
+                <Grid 
+                    container
+                    justifyContent="space-between"
+                    alignItems="stretch"
+                    spacing={3}
+                    className={classes.gridContainer}
+                >
+                    <Grid item xs={12} sm={6} md={9}>
                         <Recipes setCurrentId={setCurrentId} />
                     </Grid>
-                    <Grid item xs={12} sm={4}>
+                    <Grid item xs={12} sm={6} md={3}>
+                        <AppBar className={classes.appBarSearch} position="static" color="inherit">
+                            <TextField 
+                                name="search"
+                                variant="outlined"
+                                label="Search Recipes"
+                                onKeyPress={handleKeyPress}
+                                fullWidth
+                                value={search}
+                                onChange={(e) => setSearch(e.target.value)}
+                            />
+                            <ChipInput 
+                                style={{ margin: "10px 0" }}
+                                value={ingredients}
+                                onAdd={handleAdd}
+                                onDelete={handleDelete}
+                                label="Search by Ingredients"
+                                variant="outlined"
+                            />
+                            <Button 
+                                onClick={searchRecipe}
+                                className={classes.searchButton}
+                                variant="contained"
+                                color="primary"
+                            >
+                                Search
+                            </Button>
+                        </AppBar>
                         <Form currentId={currentId} setCurrentId={setCurrentId} />
                         <Paper elevation={6}>
                             <Pagination />
